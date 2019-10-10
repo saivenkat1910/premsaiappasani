4eac24fd07401815f676305cd40039515464322c
premsaiappasani <55195257+premsaiappasani@users.noreply.github.com>
2019-10-10 16:36:10 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,i,j,k;
+	printf("Enter the length of horizontal side");
+	scanf("%d",&a);
+	printf("Enter the length of vertical height");
+	scanf("%d",&b);
+	for(i=1;i<=b;i++)
+	{
+		for(j=a-i+1;j>0;j--)
+		{
+			printf(" ");
+		}
+		for(k=1;k<=a;k++)
+		{
+			printf(" *");
+		}
+	printf("\n");
+	}
+	return(0);
+}
